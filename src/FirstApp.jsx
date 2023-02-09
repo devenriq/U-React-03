49c3f2cf165279a62d7b18d5315d77504e2fd8f9
enrique palomino <octepc.97@gmail.com>
2023-02-09 14:46:23 -0500

    finished the counter app

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,30 +1,57 @@
+import { useState } from "react";
 import React from "react";
 import { Fragment } from "react";
 
 import PropTypes from "prop-types";
 
-export const FirstApp = ({ title, subtitle }) => {
-  if (!title) {
-    throw new Error("Title is null or undefined");
-  }
+export const FirstApp = ({ value }) => {
+  const [counter, setCounter] = useState(0);
+
+  const handleAdd = () => {
+    console.log("+1");
+    setCounter(counter + 1);
+  };
+
+  const handleReset = () => {
+    setCounter(0);
+  };
+
+  const handleRest = () => {
+    setCounter(counter - 1);
+  };
 
   return (
     <div className="flex flex-col justify-center p-5  m-5 w-1/2">
-      <h1 className="font-bold text-center text-3xl pb-3">{title}</h1>
-      <h2 className="font-bold text-center text-xl pb-3">{subtitle}</h2>
+      <h1 className="font-bold text-center text-3xl pb-3">
+        This is a Counter App
+      </h1>
+      <h2 className="font-bold text-center text-xl pb-3">{counter}</h2>
       <div className="pb-3">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
         quod! Accusantium modi quos possimus illo sed aliquam rerum illum iste
         vel ex! Doloribus nisi voluptatibus sed pariatur nostrum voluptate
         soluta!
       </div>
-      <button className="bg-blue-600 rounded rounded-full py-1 text-white">
-        Press here!
-      </button>
+      <div>
+        <button
+          className="bg-blue-600 rounded rounded-full py-1 text-white w-1/3"
+          onClick={handleAdd}
+        >
+          +1
+        </button>
+        <button
+          className="bg-blue-600 rounded rounded-full py-1 text-white w-1/3"
+          onClick={handleReset}
+        >
+          Reset
+        </button>
+        <button
+          className="bg-blue-600 rounded rounded-full py-1 text-white w-1/3"
+          onClick={handleRest}
+        >
+          -1
+        </button>
+      </div>
     </div>
   );
-};
-
-FirstApp.propTypes = {
-  title: PropTypes.string.isRequired,
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,6 @@ import { CounterApp } from "./CounterApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FirstApp title={1} subtitle={"hola"} />
+    <FirstApp value={"Hola"} />
   </React.StrictMode>
 );
